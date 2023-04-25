cbbad50162df0d17e0968e1d5627c4fef8b14c1f
Ani-John <Johnaniebiet@gmail.com>
2023-04-25 18:32:05 +0000

    fixed betty

--- printnumbers.c
+++ printnumbers.c
@@ -1,7 +1,6 @@
 #include "main.h"
 /**
- * find_the_size - Calculates the size to cast the argument
- * @i: List of arguments to be printed.
+ * find_the_size - Calculates the size to cast the argument.
  * @format: Formatted string in which to print the arguments
  * @a: a pointer to the current loop value of the format.
  * Return: size of num - as specified by precision.
